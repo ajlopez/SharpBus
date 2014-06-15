7c28988a664513e5f9e7e5c2857ea4c79fcc42f7
ajlopez <webmaster@ajlopez.com>
2014-06-15 19:42:55 -0300

    Flow first tests

---
+++ Src/SharpBus.Core.Tests/FlowTests.cs
@@ -0,0 +1,36 @@
+﻿namespace SharpBus.Core.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class FlowTests
+    {
+        [TestMethod]
+        public void CreateFlow()
+        {
+            var flow = Flow.Create();
+
+            Assert.IsNotNull(flow);
+        }
+
+        [TestMethod]
+        public void SendPayloadToEmptyFlow()
+        {
+            var flow = Flow.Create();
+
+            Assert.AreEqual(1, flow.Send(1));
+        }
+
+        [TestMethod]
+        public void ApplyTransformToInteger()
+        {
+            var flow = Flow.Create().Transform(x => ((int)x) + 1);
+
+            Assert.AreEqual(2, flow.Send(1));
+        }
+    }
+}
---
+++ Src/SharpBus.Core/Flow.cs
@@ -0,0 +1,35 @@
+﻿namespace SharpBus.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Flow
+    {
+        private IList<Func<object, object>> steps = new List<Func<object, object>>();
+
+        private Flow()
+        {
+        }
+
+        public Flow Transform(Func<object, object> transform)
+        {
+            this.steps.Add(transform);
+            return this;
+        }
+
+        public object Send(object payload)
+        {
+            foreach (var step in this.steps)
+                payload = step(payload);
+
+            return payload;
+        }
+
+        public static Flow Create()
+        {
+            return new Flow();
+        }
+    }
+}
