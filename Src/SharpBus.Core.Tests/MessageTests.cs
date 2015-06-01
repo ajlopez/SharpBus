8ceced5aa93e5d2b5f21b2fac88fd8992504d919
ajlopez <webmaster@ajlopez.com>
2015-06-01 07:33:09 -0300

    Message headers, first tests

---
+++ Src/SharpBus.Core.Tests/MessageTests.cs
@@ -0,0 +1,30 @@
+﻿namespace SharpBus.Core.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MessageTests
+    {
+        [TestMethod]
+        public void GetUnknownHeader()
+        {
+            Message message = new Message(null);
+
+            Assert.IsNull(message.Headers["Foo"]);
+        }
+
+        [TestMethod]
+        public void SetGetHeader()
+        {
+            Message message = new Message(null);
+
+            message.Headers["Foo"] = "Bar";
+
+            Assert.AreEqual("Bar", message.Headers["Foo"]);
+        }
+    }
+}
--- Src/SharpBus.Core/Message.cs
+++ Src/SharpBus.Core/Message.cs
@@ -7,6 +7,7 @@
 
     public class Message
     {
+        private MessageHeaders headers = new MessageHeaders();
         private object payload;
 
         public Message(object payload)
@@ -15,5 +16,28 @@
         }
 
         public object Payload { get { return this.payload; } internal set { this.payload = value; } }
+
+        public MessageHeaders Headers { get { return this.headers; } }
+
+        public class MessageHeaders
+        {
+            private IDictionary<string, object> values = new Dictionary<string, object>();
+
+            public object this[string name]
+            {
+                get
+                {
+                    if (this.values.ContainsKey(name))
+                        return this.values[name];
+
+                    return null;
+                }
+
+                set
+                {
+                    this.values[name] = value;
+                }
+            }
+        }
     }
 }
